f2aa0f5c7febc1fdbff6dfc7e4d7a3ea0b8bb0a9
Tejas Sawant <tejas456sawant@gmail.com>
2020-06-04 18:09:42 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={CreateRoom} />
+        <Route path="/room/:roomID" component={Room} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
